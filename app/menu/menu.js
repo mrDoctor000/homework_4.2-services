ce42332836b8c7de70e764f67e8aba82d4d427f1
Egor Ponomarev <egor@ponomarev.pp.ru>
2018-02-20 18:30:22 +0300

    menu

--- app/menu/menu.js
+++ app/menu/menu.js
@@ -1,45 +1,10 @@
 angular.module('myApp.menu', [
-    'ui.router',
-    'ngMessages'
-  ])
-  .config(function($stateProvider) {
+  'ui.router',
+  'ngMessages'
+]);
 
-    $stateProvider
-      .state({
-        name: 'list',
-        url: '/list',
-        templateUrl: 'PokemonList/PokemonList.html',
-        controller: 'PokemonListCtrl as vm'
-      })
-      .state({
-        name: 'createNewPokemon',
-        url: '/new',
-        templateUrl: 'CreatePokemon/CreatePokemon.html',
-        controller: 'CreatePokemonCtrl as vm'
-      })
-      .state({
-        name: 'detail',
-        url: '/pokemons/:pokemonId',
-        templateUrl: 'PokemonDetail/PokemonDetail.html',
-        controller: 'PokemonDetailCtrl as vm'
-      })
-      .state({
-        name: 'detail.edit',
-        url: '/edit',
-        parent: 'detail',
-        templateUrl: 'PokemonDetail/PokemonDetailEdit.html',
-        controller: function() {}
-      })
-      .state({
-        name: 'myaccount',
-        url: '/myaccount',
-        templateUrl: 'MyAccount/MyAccount.html',
-        controller: 'acconutCtrl as vm'
-      });
-  });
-
-
-angular.module('myApp.menu')
+angular
+  .module('myApp.menu')
   .controller('menuCtrl', function($scope) {
     this.menu = [
       { name: 'Список', item: 'list' },
